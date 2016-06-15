98f5f133d9d87c9504b44910d450a9865d01f526
Sara <Sara@10.18.149.72>
2016-06-15 13:21:09 -0600

    superclass main menu 6-15-16

--- murderAtTheMansion/src/byui/cit260/murderAtTheMansion/view/StartProgramView.java
+++ murderAtTheMansion/src/byui/cit260/murderAtTheMansion/view/StartProgramView.java
@@ -103,7 +103,7 @@ public class StartProgramView {
         );
         MainMenuView mainMenuView = new MainMenuView();
         
-        mainMenuView.displayMainMenuView();
+        mainMenuView.display();
         
     }
     
